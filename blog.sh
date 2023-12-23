50ed6c20a8d684c16d010cf2f9baacf1be640cb5
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-22 21:58:40 -0300

    :tada: Added the base script that will help me creating new posts

---
+++ blog.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+EDITOR="nvim"
+THEME_REPO_DIRECTORY="themes/hugo-bearcub"
+
+which git > /dev/null || echo "${0} :: git is not installed"
+
+install_theme() {
+    read -p "${0} :: <read> Enter the git repo link: " r_gitrepo
+
+    git submodule add --depth=1 "${r_gitrepo}" "${THEME_REPO_DIRECTORY}"
+    git submodule update --init --recursive
+
+}
+
+update_theme() {
+    if [ ! -e "${THEME_REPO_DIRECTORY}" ]; then
+        echo "${0} :: theme ${THEME_REPO_DIRECTORY} file doesn't exist"
+
+        read -n1 -r -p "${0} :: <read> Install theme? [Y/n] " r_user
+        [ "${r_user}" != 'n' ] &&
+            install_theme
+
+        exit
+    fi
+
+    cd "${THEME_REPO_DIRECTORY}"
+    git submodule update --remote --merge
+}
+
+new_article() {
+    ARTICLE_NAME="${1}"
+
+    DIRECTORY_PREFIX="$(date '+%y%m%d-%H%M')"
+    DIRECTORY_SUFIX="$(iconv -t ASCII//TRANSLIT <<< "${ARTICLE_NAME}" |
+        tr "[:punct:]" " " | sed -e 's/\(.*\)/\L\1/;s/ *$//;s/  */-/g')"
+
+    DIRECOTRY_NAME="${DIRECTORY_PREFIX}_${DIRECTORY_SUFIX}"
+    ARTICLE_FILE="content/post/${DIRECOTRY_NAME}/index.md"
+
+    hugo new --kind post "${ARTICLE_FILE}"
+
+    # for some reason, the sed command alone makes the target file blank...
+    mv "${ARTICLE_FILE}" "${ARTICLE_FILE}.tmp"
+    sed -e "s/{% *[Tt]itle *%}/${ARTICLE_NAME}/" "${ARTICLE_FILE}.tmp" > "${ARTICLE_FILE}"
+    rm "${ARTICLE_FILE}.tmp"
+
+    read -n1 -r -p "${0} :: <read> Open generated file? [Y/n] " r_user
+    [ "${r_user}" != 'n' ] &&
+        ${EDITOR} "${ARTICLE_FILE}"
+}
+
+case "${1}" in
+    "install-theme"|"install") install_theme; exit ;;
+    "update-theme"|"update") update_theme; exit ;;
+    "new-article"|"new") new_article "${2}"; exit ;;
+esac
